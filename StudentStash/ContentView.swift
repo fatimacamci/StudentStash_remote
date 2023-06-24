d4b4896f5266bc2bf2cd3a27aee4ad53371b762e
Fatima Camci <fatimadcamci@gmail.com>
2023-06-24 13:43:24 -0500

    Initial Commit

---
+++ StudentStash/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudentStash
+//
+//  Created by Fatima Camci on 6/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudentStash/StudentStashApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentStashApp.swift
+//  StudentStash
+//
+//  Created by Fatima Camci on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct StudentStashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
