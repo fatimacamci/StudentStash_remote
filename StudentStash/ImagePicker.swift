1236ede2ca31276f157853fff8dfee205472e552
samreenshaik22 <100487122+samreenshaik22@users.noreply.github.com>
2023-07-28 14:32:26 -0500

    Update ImagePicker.swift

--- StudentStash/ImagePicker.swift
+++ StudentStash/ImagePicker.swift
@@ -1,46 +1,49 @@
-//
-//  ImagePicker.swift
-//  StudentStash
-//
-//  Created by Fatima Camci on 7/26/23.
-//
-
 import SwiftUI
+import UIKit
+
 
 struct ImagePicker: UIViewControllerRepresentable {
-    @Binding var isVisible:Bool
     
-    func makeCoordinator () -> Coordinator {
-    Coordinator(isVisible: $isVisible)
-    }
+    @Binding var selectedImage: UIImage
+    @Environment(\.presentationMode) private var presentationMode
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
     
-    func makeUIViewController (context: Context) -> UIImagePickerController {
-        let vc = UIImagePickerController()
-        vc.allowsEditing = true
-        vc.sourceType = .photoLibrary
-        vc.delegate = context.coordinator
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
         
-        return vc
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        
+        return imagePicker
+        
     }
+    
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
-
+        //leave alone for right now
     }
-
-    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate
-    {
-        @Binding var isVisible:Bool
+    
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+     
+        var parent: ImagePicker
+     
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
         
-        init (isVisible: Binding<Bool>) {
-        _isVisible = isVisible
+    
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+     
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+     
+            parent.presentationMode.wrappedValue.dismiss()
         }
-        func imagePickerController(_ picker: UIImagePickerController,
-        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        isVisible = false
-        }
-        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        isVisible = false
-        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
     
 }
-
